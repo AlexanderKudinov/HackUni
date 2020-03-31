183149a5554989bb18926a17efeb282db12b497a
Alexander Kudinov <a.kudinov1@yandex.ru>
2020-03-31 14:40:34 +0300

    Some insignificant changes

---
+++ app/src/main/java/com/hackuniversity/partymaker/Activities/WaitingActivity.java
@@ -0,0 +1,16 @@
+package com.hackuniversity.partymaker.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.hackuniversity.partymaker.R;
+
+public class WaitingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_waiting);
+    }
+}
